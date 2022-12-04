e8660b7d581672d3d9406bf9df9ff76207b72197
bansers <451894594@qq.com>
2022-12-04 20:13:19 +0800

    1 file modified

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -1,9 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CounterService } from './counter.service';
 
-@NgModule({
-  providers: [CounterService],
-})
+@NgModule({})
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
